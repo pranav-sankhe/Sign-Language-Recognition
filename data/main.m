2fcb63af5a9faeb52116bbf10dbe427b4db7f4f8
psankhe <pranav.sankhe@jp.honda-ri.com>
2018-05-28 18:38:02 +0900

    rendering of skeleton files

---
+++ data/main.m
@@ -0,0 +1,1 @@
+read_skeleton_file('/home/user/Documents/SignLangRecog/data/nturgbd_skeletons/nturgb+d_skeletons/S001C001P002R001A037.skeleton')
--- main.py
+++ main.py
@@ -9,32 +9,6 @@ sys.path.append(os.path.abspath("/home/user/Documents/SignLangRecog/data"))
 import utils
 
 
+filepath = '/home/user/Documents/SignLangRecog/data/nturgbd_skeletons/nturgb+d_skeletons/S001C001P001R001A001.skeleton'
 
-BASE_PATH = "/home/user/Documents/SignLangRecog/data/01_CSV_full"
-
-count = 0 
-mat = []
-markers = []
-for filename in os.listdir(BASE_PATH):
-    if filename.endswith("csv"):
-        print filename
-        motionData_filepath = BASE_PATH +'/' + filename
-        motionData = utils.render_motionData(motionData_filepath)
-        # utils.plot3D(motionData, interactiveMode=True)
-        mat, markers = utils.Variance(motionData, filename)        
-        print "file count:: ", count 
-        count = count + 1
-        if count > 1000:                                           #control the number of iterations
-            break
-
-np.save('MARKER_INDICES', markers)
-# meta_filepath = "/home/user/Documents/SignLangRecog/data/position_full.xlsx"
-# motionData_filepath =  "/home/user/Documents/SignLangRecog/data/RG0_Corpus_201801_P203_02_t01.csv"
-
-# motionData = utils.render_motionData(meta_filepath, motionData_filepath)
-# # utils.plot3D(motionData, interactiveMode=True)
-# utils.meanVariance(motionData)
-
-
-# if __name__ == '__main__':
-#   tf.app.run()
+utils.readSkeletonFiles(filepath)
