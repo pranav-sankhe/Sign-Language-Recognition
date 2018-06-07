6fe90cfcb665fca916ba09ec5bd513c1a1c94997
psankhe <pranav.sankhe@jp.honda-ri.com>
2018-06-07 18:34:24 +0900

    reading video files

--- data/reading_videoData.py
+++ data/reading_videoData.py
@@ -1,73 +1,163 @@
+import os
 import cv2
 import numpy as np
 from numpy import *
 
 BATCH_SIZE = 32
 NUM_EPOCHS = 100
-IMG_HEIGHT = 220
-IMG_WIDTH = 220 
+IMG_HEIGHT = 256
+IMG_WIDTH = 256 
 IN_CHANNELS = 1
 NUM_FRAMES = 858
-BASE_PATH = ''
+BASE_PATH = '/home/data/All_video_data/HRIJ_0130_movie'
 
-filenames = os.listdir(BASE_PATH):
+l_numframe = []
+
+filepath = '/home/data/02_CSV_REAL/01_CSV_full/memo.txt'
+f = open(filepath, "r")                     # Open the file
+for line in f:
+    l_numframe.append(int(line[len(line) - 5:len(line)]))
+
+filenames = os.listdir(BASE_PATH)
 filenames = filenames[0:BATCH_SIZE*NUM_EPOCHS]
 def get_batch(step):
 
     filename_batch = filenames[step: (step+1)*BATCH_SIZE]
-    data = np.zeros((BATCH_SIZE, NUM_FRAMES, IMG_HEIGHT, IMG_WIDTH, IN_CHANNELS))
-    cap = cv2.VideoCapture("test.mp4")
+    vid_data = np.zeros((BATCH_SIZE, NUM_FRAMES, IMG_HEIGHT, IMG_WIDTH, IN_CHANNELS))
+    opFlow_data = np.zeros((BATCH_SIZE, NUM_FRAMES, IMG_HEIGHT, IMG_WIDTH, IN_CHANNELS))    
+    
 
-    ret, frame1 = cap.read()
-    #frame1 = cv2.resize(frame1, (220, 220))
-    prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
-    hsv = np.zeros_like(frame1)
-    hsv[...,1] = 255
-    count = 0 
+    for i in range(BATCH_SIZE):
+        cap = cv2.VideoCapture(BASE_PATH + '/' + filename_batch[i])
+        print "Reading file", filename_batch[i]
+        ret, frame1 = cap.read()
+        frame1_save = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+        frame1_save = cv2.resize(frame1_save, (IMG_HEIGHT, IMG_WIDTH))
+        count = 0 
+        vid_data[i,count,:,:,0] = frame1_save
+        opFlow_data[i,count,:,:,0] = frame1_save
 
-    while(cap.isOpened() == True):
-        ret, frame2 = cap.read()
-        if ret == False: 
-            break
-        next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-        prvs = cv2.resize(prvs, (220, 220))
-        next = cv2.resize(next, (220, 220))
-        
-            
-        flow = cv2.calcOpticalFlowFarneback(prvs,next,0.5, 1, 3, 15, 3, 5, 0)
-        #flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 10, 3, 5, 1.2, 0)
-
-
-        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-        hsv = cv2.resize(hsv, (220, 220))
-
-        hsv[...,0] = ang*180/np.pi/2
-        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-        rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-    #    cv2.imshow('frame2',rgb)
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
-        elif k == ord('s'):
-            cv2.imwrite('opticalfb.png',frame2)
-            cv2.imwrite('opticalhsv.png',rgb)
-        prvs = next
+        prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+        hsv = np.zeros_like(frame1)
+        hsv[...,1] = 255
         count = count + 1
-    print count
-    cap.release()
 
 
-# count = 0 
-# while(1):
+        while(cap.isOpened() == True):
+            print "At frame", count
+            ret, frame2 = cap.read()
+            frame2_save = cv2.resize(frame1_save, (IMG_HEIGHT, IMG_WIDTH))
+            vid_data[i,count,:,:,0] = frame2_save
 
-#     ret, frame2 = cap.read()
-#     if ret == False: 
-#         break
+            if ret == False: 
+                break
+            next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+            prvs = cv2.resize(prvs, (IMG_HEIGHT, IMG_WIDTH))
+            next = cv2.resize(next, (IMG_HEIGHT, IMG_WIDTH))
+            
+                
+            flow = cv2.calcOpticalFlowFarneback(prvs,next,0.5, 1, 3, 15, 3, 5, 0)
+            #flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 10, 3, 5, 1.2, 0)
 
-#     count = count + 1
 
-# print count     
+            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+            hsv = cv2.resize(hsv, (IMG_HEIGHT, IMG_WIDTH))
+
+            hsv[...,0] = ang*180/np.pi/2
+            hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+            rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+            rgb = cv2.cvtColor(rgb,cv2.COLOR_BGR2GRAY)
+
+            opFlow_data[i,count,:,:,0] = rgb
+            # cv2.imshow('frame2',rgb)
+            # k = cv2.waitKey(30) & 0xff
+            # if k == 27:
+            #     break
+            # elif k == ord('s'):
+            #     cv2.imwrite('opticalfb.png',frame2)
+            #     cv2.imwrite('opticalhsv.png',rgb)
+            prvs = next
+            count = count + 1
+        print vid_data[i,:,:,:,:].shape
+        print opFlow_data[i,:,:,:,:].shape    
+        cap.release()
+    return vid_data, opFlow_data
 
 
-cap.release()
+
+def save_as_numpy():
+    if not os.path.exists('npVideos'):
+        os.makedirs('npVideos')
+    if not os.path.exists('npopFlow'):
+        os.makedirs('npopFlow')        
+
+    for i in range(len(filenames)):
+
+        vid_data = np.zeros((NUM_FRAMES, IMG_HEIGHT, IMG_WIDTH, IN_CHANNELS))
+        opFlow_data = np.zeros((NUM_FRAMES, IMG_HEIGHT, IMG_WIDTH, IN_CHANNELS))    
+
+        cap = cv2.VideoCapture(BASE_PATH + '/' + filenames[i])
+        print "Reading file", filenames[i]
+        ret, frame1 = cap.read()
+        frame1_save = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+        frame1_save = cv2.resize(frame1_save, (IMG_HEIGHT, IMG_WIDTH))
+        count = 0 
+        vid_data[count,:,:,0] = frame1_save
+        opFlow_data[count,:,:,0] = frame1_save
+
+        prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+        hsv = np.zeros_like(frame1)
+        hsv[...,1] = 255
+        count = count + 1
+
+
+        while(cap.isOpened() == True):
+            print "At frame", count
+            ret, frame2 = cap.read()
+            frame2_save = cv2.resize(frame1_save, (IMG_HEIGHT, IMG_WIDTH))
+            vid_data[count,:,:,0] = frame2_save
+
+            if ret == False: 
+                break
+            next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+            prvs = cv2.resize(prvs, (IMG_HEIGHT, IMG_WIDTH))
+            next = cv2.resize(next, (IMG_HEIGHT, IMG_WIDTH))
+                
+            flow = cv2.calcOpticalFlowFarneback(prvs,next,0.5, 1, 3, 15, 3, 5, 0)
+            #flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 10, 3, 5, 1.2, 0)
+
+
+            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+            hsv = cv2.resize(hsv, (IMG_HEIGHT, IMG_WIDTH))
+
+            hsv[...,0] = ang*180/np.pi/2
+            hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+            rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+            rgb = cv2.cvtColor(rgb,cv2.COLOR_BGR2GRAY)
+
+            opFlow_data[count,:,:,0] = rgb
+            # cv2.imshow('frame2',rgb)
+            # k = cv2.waitKey(30) & 0xff
+            # if k == 27:
+            #     break
+            # elif k == ord('s'):
+            #     cv2.imwrite('opticalfb.png',frame2)
+            #     cv2.imwrite('opticalhsv.png',rgb)
+            prvs = next
+            count = count + 1
+            filename = filenames[i].split('.')[0]
+        
+        vid_data = vid_data[::2, :, :, :]
+        opFlow_data = opFlow_data[::2, :, :, :]
+        np.save('npVideos' + '/' + filenames[i], vid_data)
+        np.save('npopFlow' + '/' + filenames[i], opFlow_data)
+        
+        cap.release()
+    
+
+save_as_numpy()
+
+
+
 cv2.destroyAllWindows()
+
--- data/test.py
+++ data/test.py
@@ -1,48 +1,231 @@
+import tensorflow as tf
 import cv2
 import numpy as np
-from numpy import *
-cap = cv2.VideoCapture("test.mp4")
+NUM_EPOCHS = 1
+SEQ_NUM_FRAMES = 291
 
-ret, frame1 = cap.read()
-#frame1 = cv2.resize(frame1, (220, 220))
-prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
-hsv = np.zeros_like(frame1)
-hsv[...,1] = 255
-count = 0 
 
-while(cap.isOpened() == True):
-    ret, frame2 = cap.read()
-    if ret == False: 
-        break
-    next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-    prvs = cv2.resize(prvs, (220, 220))
-    next = cv2.resize(next, (220, 220))
+# def decode(serialized_example, sess):
+#   # Prepare feature list; read encoded JPG images as bytes
+#   features = dict()
+#   features["class_label"] = tf.FixedLenFeature((), tf.int64)
+#   features["frames"] = tf.VarLenFeature((), tf.string)
+#   features["num_frames"] = tf.FixedLenFeature((), tf.int64)
+
+#   # Parse into tensors
+#   parsed_features = tf.parse_single_example(serialized_example, features)
+
+#   # Randomly sample offset from the valid range.
+#   random_offset = tf.random_uniform(
+#       shape=(), minval=0,
+#       maxval=parsed_features["num_frames"] - SEQ_NUM_FRAMES, dtype=tf.int64)
+
+#   offsets = tf.range(offset, offset + SEQ_NUM_FRAMES)
+
+#   # Decode the encoded JPG images
+#   images = tf.map_fn(lambda i: tf.image.decode_jpeg(parsed_features["frames"][i]),
+#                      offsets)
+
+#   label  = tf.cast(parsed_features["class_label"], tf.int64)
+
+#   return images, label
+
+
+# def video_left_right_flip(images):
+#     '''
+#     Performs tf.image.flip_left_right on entire list of video frames.
+#     Work around since the random selection must be consistent for entire video
+#     :param images: Tensor constaining video frames (N,H,W,3)
+#     :return: images: Tensor constaining video frames left-right flipped (N,H,W,3)
+#     '''
+#     images_list = tf.unstack(images)
+#     for i in range(len(images_list)):
+#         images_list[i] = tf.image.flip_left_right(images_list[i])
+#     return tf.stack(images_list)
+
+# def preprocess_video(images, label):
+#     '''
+#     Given the 'images' Tensor of video frames (N,H,W,3) perform the following
+#     preprocessing steps:
+#     1. Takes a random crop of size CROP_SIZExCROP_SIZE from the video frames.
+#     2. Optionally performs random left-right flipping of the video.
+#     3. Performs video normalization, to the range [-0.5, +0.5]
+#     :param images: Tensor (tf.uint8) constaining video frames (N,H,W,3)
+#     :param label:  Tensor (tf.int64) constaining video frames ()
+#     :return:
+#     '''
+
+#     # Take a random crop of the video, returns tensor of shape (N,CROP_SIZE,CROP_SIZE,3)
+#     images = tf.random_crop(images, (SEQ_NUM_FRAMES, CROP_SIZE, CROP_SIZE, 3))
+
+#     if RANDOM_LEFT_RIGHT_FLIP:
+#         # Consistent left_right_flip for entire video
+#         sample = tf.random_uniform(shape=[], minval=0, maxval=1, dtype=tf.float32)
+#         option = tf.less(sample, 0.5)
+#         images = tf.cond(option,
+#                          lambda: video_left_right_flip(images),
+#                          lambda: tf.identity(images))
+
+#     # Normalization: [0, 255] => [-0.5, +0.5] floats
+#     images = tf.cast(images, tf.float32) * (1./255.) - 0.5
+#     return images, label
+
+BATCH_SIZE = 1
+def decode(serialized_example, sess):
+  # Prepare feature list; read encoded JPG images as bytes
+  features = dict()
+  features["class_label"] = tf.FixedLenFeature((), tf.int64)
+  features["frames"] = tf.VarLenFeature((), tf.string)
+  features["num_frames"] = tf.FixedLenFeature((), tf.int64)
+
+  # Parse into tensors
+  parsed_features = tf.parse_single_example(serialized_example, features)
+
+  # Randomly sample offset from the valid range.
+  random_offset = tf.random_uniform(
+      shape=(), minval=0,
+      maxval=parsed_features["num_frames"] - SEQ_NUM_FRAMES, dtype=tf.int64)
+
+  offsets = tf.range(offset, offset + SEQ_NUM_FRAMES)
+
+  # Decode the encoded JPG images
+  images = tf.map_fn(lambda i: tf.image.decode_jpeg(parsed_features["frames"][i]),
+                     offsets)
+
+ # label  = tf.cast(parsed_features["class_label"], tf.int64)
+
+  return images#, label
+
+
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_list_feature(values):
+    """Wrapper for inserting bytes features into Example proto."""
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+with tf.python_io.TFRecordWriter('tftest') as writer:
+
+  # Read and resize all video frames, np.uint8 of size [N,H,W,3]
+  frames = np.load('./npVideos/RG0_Corpus_201801_P150_02_t01.mp4.npy')
+
+  features = {}
+  features['num_frames']  = _int64_feature(frames.shape[0])
+  features['height']      = _int64_feature(frames.shape[1])
+  features['width']       = _int64_feature(frames.shape[2])
+  features['channels']    = _int64_feature(frames.shape[3])
+#  features['class_label'] = _int64_feature(example['class_id'])
+#  features['class_text']  = _bytes_feature(tf.compat.as_bytes(example['class_label']))
+#  features['filename']    = _bytes_feature(tf.compat.as_bytes(example['video_id']))
+
+  # Compress the frames using JPG and store in as a list of strings in 'frames'
+  encoded_frames = [tf.compat.as_bytes(cv2.imencode(".jpg", frame)[1].tobytes())
+                    for frame in frames]
+  features['frames'] = _bytes_list_feature(encoded_frames)
+
+  tfrecord_example = tf.train.Example(features=tf.train.Features(feature=features))
+  writer.write(tfrecord_example.SerializeToString())
+
+
+if  __name__ == "__main__":
+
+    import glob
+    tfrecord_files = glob.glob("tftest")
+    tfrecord_files.sort()
+
+    sess = tf.Session()
+    init_op = tf.group(
+        tf.global_variables_initializer(),
+        tf.local_variables_initializer())
+
+    dataset = tf.data.TFRecordDataset(tfrecord_files)
+
+    dataset = dataset.repeat(NUM_EPOCHS)
+    temp = decode(dataset, sess)
+    dataset = dataset.map(temp)
+    dataset = dataset.map(preprocess_video)
+
+    # The parameter is the queue size
+    dataset = dataset.shuffle(1000 + 3 * BATCH_SIZE)
+    dataset = dataset.batch(BATCH_SIZE)
+
+    iterator = dataset.make_one_shot_iterator()
+    next_batch = iterator.get_next()
+
+    sess.run(init_op)
+
+    while True:
+
+        # Fetch a new batch from the dataset
+        batch_videos, batch_labels = sess.run(next_batch)
+
+        for sample_idx in range(BATCH_SIZE):
+            print("Class label = {}".format(batch_labels[sample_idx]))
+            for frame_idx in range(SEQ_NUM_FRAMES):
+                cv2.imshow("image", batch_videos[sample_idx,frame_idx])
+                cv2.waitKey(20)
+            key = cv2.waitKey(0)
+            if key == ord('q'):
+                exit()
+
+
+
+
+
+# import cv2 
+# import numpy as np 
+
+
+# cap = cv2.VideoCapture('test.mp4')
+# count = 0 
+# print cap
+# while(cap.isOpened() == True):
+
     
-        
-    flow = cv2.calcOpticalFlowFarneback(prvs,next,0.5, 1, 3, 15, 3, 5, 0)
-    #flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 10, 3, 5, 1.2, 0)
-
-
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv = cv2.resize(hsv, (220, 220))
-
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-#    cv2.imshow('frame2',rgb)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-    elif k == ord('s'):
-        cv2.imwrite('opticalfb.png',frame2)
-        cv2.imwrite('opticalhsv.png',rgb)
-    prvs = next
-    count = count + 1
-print count
-cap.release()
-cv2.destroyAllWindows()
+#     ret, frame = cap.read()
+#     if ret == False:
+#         print "video ended or not detected"
+#         break
+#     frame = cv2.resize(frame, (256, 256))
+#     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+#     count = str(count)
+#     num = '' 
+#     diff = 3 - len(count)
+#     if diff == 0:
+#         pass
+#     else:
+#         for i in range(diff):
+#             num = num + '0'
+    
+#     num = num + count   
+#     if int(count)%2 == 0:
+#         cv2.imwrite('test' + '/' + num + '.jpg', frame) 
+#     count = int(count)
+#     count = count + 1    
 
 
 
-cap.release()
-cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
