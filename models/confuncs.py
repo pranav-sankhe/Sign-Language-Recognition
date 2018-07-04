d717161a386b5af177e309ba66e3cb7d22465a44
psankhe <pranav.sankhe@jp.honda-ri.com>
2018-07-04 12:12:15 +0900

    convinient functions for CNN arch

---
+++ models/confuncs.py
@@ -0,0 +1,10 @@
+import tensorflow
+
+def batch_norm(inputs, training, data_format):
+  """Performs a batch normalization using a standard set of parameters."""
+  # We set fused=True for a significant performance boost. See
+  # https://www.tensorflow.org/performance/performance_guide#common_fused_ops
+  return tf.layers.batch_normalization(
+      inputs=inputs, axis=1 if data_format == 'channels_first' else 3,
+      momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
+      scale=True, training=training, fused=True)
