2373958cc9bcf76deb447083511eadb5e69a6c16
psankhe <pranav.sankhe@jp.honda-ri.com>
2018-07-05 19:27:08 +0900

    added processing for pretraining dataset

--- data/reading_videoData.py
+++ data/reading_videoData.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
 import os
 import cv2
 import numpy as np
@@ -20,19 +18,16 @@ BASE_PATH = '/home/data/All_video_data/HRIJ_0130_movie'
 l_numframe = []
 
 filepath = '/home/data/02_CSV_REAL/01_CSV_full/memo.txt'
-f = open(filepath, "r")                     # Open the file
-for line in f:
-    l_numframe.append(int(line[len(line) - 5:len(line)]))
+# f = open(filepath, "r")                     # Open the file
+# for line in f:
+#     l_numframe.append(int(line[len(line) - 5:len(line)]))
 
 filenames = os.listdir(BASE_PATH)
 filenames = filenames[0:BATCH_SIZE*NUM_EPOCHS]
 
-def save_as_jpg():
+def save_frames_hri():
     if not os.path.exists('video_frames'):
         os.makedirs('video_frames')
-    if not os.path.exists('opflow_frames'):
-        os.makedirs('opflow_frames')
-
 
     for i in range(len(filenames)):
         if filenames[i].split('.')[1] != 'mp4':
@@ -40,8 +35,6 @@ def save_as_jpg():
         if not os.path.exists('video_frames' + '/' + filenames[i].split('.')[0]):
             os.makedirs('video_frames' + '/' + filenames[i].split('.')[0])
 
-        if not os.path.exists('opflow_frames' + '/' + filenames[i].split('.')[0]):
-            os.makedirs('opflow_frames' + '/' + filenames[i].split('.')[0])
             
         cap = cv2.VideoCapture(BASE_PATH + '/' + filenames[i])
         print "Reading file", filenames[i], i 
@@ -59,14 +52,7 @@ def save_as_jpg():
                 num = num + '0'
         
         num = num + str(count)   
-        if int(count)%2 == 0:
-            #print 'video_frames' + '/' + filenames[i].split('.')[0] + '/' +  num + '.jpg', i
-            cv2.imwrite('video_frames' + '/' + filenames[i].split('.')[0] + '/' +  num + '.jpg', frame1_save)
-            cv2.imwrite('opflow_frames' + '/' + filenames[i].split('.')[0] + '/' + num + '.jpg', np.random.rand(256,256,3))
-        
-        prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
-        hsv = np.zeros_like(frame1)
-        hsv[...,1] = 255
+        cv2.imwrite('video_frames' + '/' + filenames[i].split('.')[0] + '/' +  num + '.jpg', frame1_save)        
         count = count + 1
 
 
@@ -88,41 +74,12 @@ def save_as_jpg():
                     num = num + '0'
             
             num = num + str(count)   
-            if int(count)%2 == 0:
+            if int(count)%8 == 0:
                 cv2.imwrite('video_frames' + '/' + filenames[i].split('.')[0] + '/'  + num + '.jpg', frame2_save)
 
-            next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-            prvs = cv2.resize(prvs, (IMG_HEIGHT, IMG_WIDTH))
-            next = cv2.resize(next, (IMG_HEIGHT, IMG_WIDTH))
-                
-            flow = cv2.calcOpticalFlowFarneback(prvs,next,0.5, 1, 3, 15, 3, 5, 0)
-
-
-            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-            hsv = cv2.resize(hsv, (IMG_HEIGHT, IMG_WIDTH))
-
-            hsv[...,0] = ang*180/np.pi/2
-            hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-            rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-                       
-            num = '' 
-            diff = 3 - len(str(count))
-            if diff == 0:
-                pass
-            else:
-                for j in range(diff):
-                    num = num + '0'
-            
-            num = num + str(count)
-            if int(count)%2 == 0:
-                #print "opflow",'opflow_frames' + '/' + filenames[i].split('.')[0] + '/' + num + '.jpg', i 
-                cv2.imwrite('opflow_frames' + '/' + filenames[i].split('.')[0] + '/' + num + '.jpg', rgb)
-
-            prvs = next
             count = count + 1
-            filename = filenames[i].split('.')[0]
        
-        
+        return
         cap.release()
 
 
@@ -292,3 +249,4 @@ def savefile_pretrain():
                     if cv2.waitKey(10) == 27:                     
                         break
 
+save_frames_hri()
